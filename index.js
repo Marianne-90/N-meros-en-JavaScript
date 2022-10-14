316358dbc27040e9fede97a009c6eee7eed32c5a
Marianne Garrido <64554585+Marianne-90@users.noreply.github.com>
2022-10-14 21:10:05 +0000

    valor max

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let alturaCentimetros = 165
 let alturaMetros = 1.65
 let peso = 52.9
-let alturaRedondeada = Math.round(alturaMetros+.5)
+let alturaRedondeada = Math.round(alturaMetros*10+.5)/10
 let pesoRedondeado = Math.round(peso-.5)
+
+let valorMax= Number.MAX_VALUE
+
+console.log(valorMax===(valorMax+1))
